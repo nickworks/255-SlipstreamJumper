f8b9a6e7e36fd0ead9b4ae2fd3d08737aa959ffe
Michael Caughman <caughmmg1993@gmail.com>
2019-11-03 17:03:31 -0500

    Followed Videos on Player Movment and created Scene
    +Player Movement
    +scripts PlayerMovement, MainHUD, Game, AABB

--- Assets/Caughman/Scripts/Zone.cs
+++ Assets/Caughman/Scripts/Zone.cs
@@ -6,8 +6,8 @@ namespace Caughman {
     public class Zone : Pattison.Zone {
 
         new static public ZoneInfo info = new ZoneInfo() {
-            zoneName = "The Water Temple",
-            creator = "Student Lastname",
+            zoneName = "Spikeville",
+            creator = "Caughman",
             level = "CaughmanScene"
         };
 
