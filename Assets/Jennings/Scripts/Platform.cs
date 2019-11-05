8b23a10a97ca5f4a104782c47912de7583c89542
Unknown <jaylen_jennings@yahoo.com>
2019-11-05 15:03:42 -0500

    Improved Platforms

---
+++ Assets/Jennings/Scripts/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jennings {
+
+    [RequireComponent(typeof(AABB))]
+    public class Platform : MonoBehaviour {
+        
+
+        void Start()
+        {
+            // register self:
+            AABB aabb = GetComponent<AABB>();
+            Zone.platforms.Add(aabb);
+        }
+        void OnDestroy()
+        {
+            AABB aabb = GetComponent<AABB>();
+            Zone.platforms.Remove(aabb);
+        }
+
+    }
+}
